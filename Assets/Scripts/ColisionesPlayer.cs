cb006a2e6f9be3fdd4b7c4bd1081d624da8316cf
ojoninja03 <Unacuentam126@gmail.com>
2025-05-18 20:05:27 -0600

    script

--- Assets/Scripts/ColisionesPlayer.cs
+++ Assets/Scripts/ColisionesPlayer.cs
@@ -5,5 +5,45 @@ using System.Collections.Generic;
 
 public class ColisionesPlayer : MonoBehaviour
 {
-   
+    public AudioSource reproductor;
+    public AudioClip agua;
+    public Image donkeykong;
+    public GameObject video;
+
+
+    void Start()
+    {
+        donkeykong.gameObject.SetActive(false);
+        video.SetActive(false);
+
+    }
+
+    private void OnTriggerEnter(Collider obj)
+    {
+        if (obj.tag == "fuente")
+        {
+            reproductor.PlayOneShot(agua);
+        }
+        if (obj.tag == "comida")
+        {
+            donkeykong.gameObject.SetActive(true);
+        }
+        if (obj.tag == "cine")
+        {
+            video.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider obj)
+    {
+        reproductor.Stop();
+        if (obj.tag == "comida")
+        {
+            donkeykong.gameObject.SetActive(false);
+        }
+        if (obj.tag == "cine")
+        {
+            video.SetActive(false);
+        }
+
+    }
 }
